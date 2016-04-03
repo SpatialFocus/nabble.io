4f1b8befb21667313e04daa779bc27e8d4c45393
Christoph Perger <christoph.perger@spatial-focus.net>
2016-04-03 21:56:14 +0200

    Added BrowserLink support

--- src/Nabble.Web/Startup.cs
+++ src/Nabble.Web/Startup.cs
@@ -2,17 +2,39 @@
 {
 	using Microsoft.AspNet.Builder;
 	using Microsoft.AspNet.Hosting;
+	using Microsoft.Extensions.Configuration;
 	using Microsoft.Extensions.DependencyInjection;
 
 	public class Startup
 	{
+		public Startup(IHostingEnvironment env)
+		{
+			// Set up configuration sources.
+			IConfigurationBuilder builder =
+				new ConfigurationBuilder().AddJsonFile("appsettings.json")
+					.AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true);
+
+			builder.AddEnvironmentVariables();
+			Configuration = builder.Build();
+		}
+
+		public IConfigurationRoot Configuration { get; set; }
+
 		// Entry point for the application.
 		public static void Main(string[] args) => WebApplication.Run<Startup>(args);
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-		public void Configure(IApplicationBuilder app)
+		public void Configure(IApplicationBuilder app, IHostingEnvironment env)
 		{
+			if (env.IsDevelopment())
+			{
+				app.UseBrowserLink();
+				app.UseDeveloperExceptionPage();
+			}
+
 			app.UseStaticFiles();
+
+			app.UseIISPlatformHandler();
 
 			app.UseMvc(
 				r =>
@@ -20,10 +42,6 @@
 					r.MapRoute(name: "default", template: "{controller}/{action}/{id?}",
 						defaults: new { controller = "Home", action = "Index" });
 				});
-
-			////app.UseIISPlatformHandler();
-
-			////app.Run(async (context) => { await context.Response.WriteAsync("Hello World!"); });
 		}
 
 		// This method gets called by the runtime. Use this method to add services to the container.
