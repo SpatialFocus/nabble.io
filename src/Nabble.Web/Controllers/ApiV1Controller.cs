903b5d981e0c2a8175f16d321067eeaea19739a4
Christoph Perger <christoph.perger@spatial-focus.net>
2016-04-05 20:00:48 +0200

    Image delivery demo in API Get

--- src/Nabble.Web/Controllers/ApiV1Controller.cs
+++ src/Nabble.Web/Controllers/ApiV1Controller.cs
@@ -1,13 +1,22 @@
 ﻿namespace Nabble.Web.Controllers
 {
+	using System.IO;
+	using Microsoft.AspNet.Hosting;
 	using Microsoft.AspNet.Mvc;
 
 	[Route("api/v1")]
 	public class ApiV1Controller : Controller
 	{
+		private readonly IHostingEnvironment environment;
+
+		public ApiV1Controller(IHostingEnvironment environment)
+		{
+			this.environment = environment;
+		}
+
 		// GET: api/v1
 		[HttpGet]
-		public ActionResult Get()
+		public IActionResult Get()
 		{
 			return HttpBadRequest("Usage: /api/v1/{vendor}/{account}/{project}/{branch (opt)}/{analyzer}?{parameters}");
 		}
@@ -15,38 +24,19 @@
 		// GET api/v1/appveyor/cperger/demoproject1/development/fxcop
 		[HttpGet("{vendor}/{account}/{project}/{branch}/{analyzer}")]
 		[HttpGet("{vendor}/{account}/{project}/{analyzer}")]
-		public string Get(string vendor, string account, string project, string analyzer, string branch = null,
+		public IActionResult Get(string vendor, string account, string project, string analyzer, string branch = null,
 			[FromQuery] string parameters = null)
 		{
-			// TODO: All the magic to map parameters to CORE functions
+			// TODO: All the magic to map parameters to CORE functions that returns the local path to the generated/cached file (svg, png, ...)
+			string localpath = "images/favicon/favicon-96x96.png";
 
 			// Write image to response stream: http://stackoverflow.com/questions/5629251/c-sharp-outputting-image-to-response-output-stream-giving-gdi-error
-			////using (Bitmap image = new Bitmap(context.Server.MapPath("images/stars_5.png")))
-			////{
-			////	using (MemoryStream ms = new MemoryStream())
-			////	{
-			////		image.Save(ms, System.Drawing.Imaging.ImageFormat.Png);
-			////		context.Response.ContentType = "image/png";
-			////		ms.WriteTo(context.Response.OutputStream);
-			////	}
-			////}
-
 			// Or maybe: https://jamessdixon.wordpress.com/2013/10/01/handling-images-in-webapi/
-			////public HttpResponseMessage Get(int id)
-			////{
-			////	var result = new HttpResponseMessage(HttpStatusCode.OK);
-			////	String filePath = HostingEnvironment.MapPath("~/Images/HT.jpg");
-			////	FileStream fileStream = new FileStream(filePath, FileMode.Open);
-			////	Image image = Image.FromStream(fileStream);
-			////	MemoryStream memoryStream = new MemoryStream();
-			////	image.Save(memoryStream, ImageFormat.Jpeg);
-			////	result.Content = new ByteArrayContent(memoryStream.ToArray());
-			////	result.Content.Headers.ContentType = new MediaTypeHeaderValue("image/jpeg");
+			string fullpath = Path.Combine(this.environment.WebRootPath, localpath);
 
-			////	return result;
-			////}
+			Stream badgeStream = new FileStream(fullpath, FileMode.Open);
 
-			return "value " + vendor;
+			return new FileStreamResult(badgeStream, "image/png");
 		}
 	}
 }
