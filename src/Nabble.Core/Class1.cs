f71659e1e823432a3cad93bc1dcfdf8c91b6a625
Christopher Dresel <christopher.dresel@spatial-focus.net>
2016-04-08 12:38:37 +0200

    Configured StyleCop, applied code style

--- src/Nabble.Core/Class1.cs
+++ src/Nabble.Core/Class1.cs
@@ -1,12 +1,22 @@
-﻿namespace Nabble.Core
+﻿// <copyright file="Class1.cs" company="Spatial Focus GmbH">
+// Copyright (c) Spatial Focus GmbH. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+
+namespace Nabble.Core
 {
 	using System.IO;
 
-	// This violates CA1001
+	/// <summary>
+	/// This class violates CA1001
+	/// </summary>
 	public class Class1
 	{
-		FileStream newFile;
+		private FileStream newFile;
 
+		/// <summary>
+		/// Initializes a new instance of the <see cref="Class1" /> class.
+		/// </summary>
 		public Class1()
 		{
 			this.newFile = new FileStream(@"c:\temp.txt", FileMode.Open);
