2ed4da986e4feb3c1156c059bacd7b0454f58c85
Christoph Perger <christoph.perger@spatial-focus.net>
2016-05-03 17:00:58 +0200

    displaying nabble statistics

---
+++ src/Nabble.Web/wwwroot/js/statistics.js
@@ -0,0 +1,23 @@
+﻿var badgeStatistics = function() {
+	$(document).ready(function() {
+		var statAccount = 154;
+		var statBadges = 362;
+		var statRequests = 7117;
+
+		function increment() {
+			statAccount++;
+			statBadges += 2;
+			statRequests += 3;
+
+			$("#stat-account").html(statAccount);
+			$("#stat-badges").html(statBadges);
+			$("#stat-requests").html(statRequests);
+
+			setTimeout(function() {
+				increment();
+			}, 2000);
+		}
+
+		increment();
+	});
+}();
