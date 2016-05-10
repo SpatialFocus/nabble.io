c1b1fe4cbd2ed675deab266f72e5458dc043aa20
Christopher Dresel <christopher.dresel@spatial-focus.net>
2016-05-10 16:53:29 +0200

    Add custom NoCache attribute for ApiV1Controller

---
+++ src/Nabble.Web/Core/NoCacheAttribute.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNet.Mvc.Filters;
+
+namespace Nabble.Web.Core
+{
+	public class NoCacheAttribute : ActionFilterAttribute
+	{
+		public override void OnActionExecuted(ActionExecutedContext context)
+		{
+			base.OnActionExecuted(context);
+
+			context.HttpContext.Response.Headers.Add("Cache-Control", "no-cache");
+			context.HttpContext.Response.Headers.Add("Expires", "-1");
+		}
+	}
+}
